a90c8b5ccfc9eb690091a16449062b1608a14203
duckyoon <dkdlel616@gmail.com>
2025-07-21 23:29:39 +0900

    FEAT: POST 메서드 개선
    ---
    ---

--- src/main/java/com/junes/rest/webservices/restful_web_services/user/UserResource.java
+++ src/main/java/com/junes/rest/webservices/restful_web_services/user/UserResource.java
@@ -1,7 +1,10 @@
 package com.junes.rest.webservices.restful_web_services.user;
 
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
+import java.net.URI;
 import java.util.List;
 
 @RestController
@@ -27,9 +30,20 @@ public class UserResource {
 
     //POST /users
     @PostMapping("/users")
-    public void createUser(@RequestBody User user){
+    public ResponseEntity<User> createUser(@RequestBody User user){
+
+        User savedUser = service.save(user);
         // Postman 에서 post 테스트 가능
-        service.save(user);
+
+        // http://localhost:8080/users/{id} 형태의 URI 생성
+        URI location = ServletUriComponentsBuilder.fromCurrentRequest()
+                .path("/{id}")
+                .buildAndExpand(savedUser.getId())
+                .toUri();
+
+        // HTTP 상태코드 201 Created 와 함께 응답을 반환
+        // location 헤더에 새로 생성된 리소스의 URI 가 포함됨.
+        return ResponseEntity.created(location).build();
     }
 
 }
